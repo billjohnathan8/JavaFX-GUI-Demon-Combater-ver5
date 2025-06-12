38d66d5e9b57ec9f0aae793a34610c22cc245506
BarracudaBill <paladingod8@gmail.com>
2025-06-12 18:06:41 +0800

    skeleton for special stat rolling strategies in future (Munchkin's Method for Point Buy and NHe Party Roll for Group Character Creation)

---
+++ app/src/main/java/guidemon/engine/stats/strategies/unique/NHEPartyRollStrategy.java
@@ -0,0 +1,22 @@
+// package guidemon.engine.stats.strategies.unique;
+
+// import guidemon.engine.stats.IStatRollingStrategy;
+
+//wip: requires group character creation
+// //roll all party stats together. each person takes 1 dump stat. each person takes 1 god stat 
+// public class NHEPartyRollStrategy implements IStatRollingStrategy {
+//     @Override 
+//     public int[] rollStats() {
+//         return null; 
+//     } 
+
+//     @Override 
+//     public String getName() { 
+//         return "NHE Party Roll"; 
+//     }  
+
+//     @Override
+//     public String toString() {
+//         return getName();
+//     }
+// }
