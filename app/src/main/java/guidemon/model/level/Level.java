aa42e439c8d4a7b88150bec591fb5d082ffe570f
BarracudaBill <paladingod8@gmail.com>
2025-06-12 18:00:54 +0800

    levelling feature skeleton classes - ver0

---
+++ app/src/main/java/guidemon/model/level/Level.java
@@ -0,0 +1,20 @@
+package guidemon.model.level;
+
+// import java.util.List;
+// import java.util.Map;
+
+// import guidemon.model.Ability; 
+
+
+//TODO: to be implemented
+public class Level {
+    // private String name; 
+    // private LevelType levelType; 
+    // private int currentLevel; 
+
+    // private Map<Integer, List<Ability>> abilityProgression; 
+
+    // public Level() { 
+
+    // }
+}
---
+++ app/src/main/java/guidemon/model/level/LevelType.java
@@ -0,0 +1,8 @@
+package guidemon.model.level;
+
+public enum LevelType {
+    MAJOR_CLASS,
+    MINOR_CLASS,
+    RACIAL,
+    JOB_CLASS;
+}
