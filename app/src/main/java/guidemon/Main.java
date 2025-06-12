235c10fae4d6d75a5586647db9c1c93aff692289
BarracudaBill <paladingod8@gmail.com>
2025-06-12 18:03:22 +0800

    changed main to accomodate fot stat rolling feature ver1

--- app/src/main/java/guidemon/Main.java
+++ app/src/main/java/guidemon/Main.java
@@ -2,19 +2,16 @@ package guidemon;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-// import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainView.fxml"));
-        Scene scene = new Scene(root);
-        stage.setTitle("FXML Window");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainView.fxml"));
+        Scene scene = new Scene(loader.load());
         stage.setScene(scene);
+        stage.setTitle("Stat Roller");
         stage.show();
     }
 
