76b28781f464d0cf822a8a3312c2bc2e461c00be
BarracudaBill <paladingod8@gmail.com>
2025-07-03 13:52:33 +0800

    changed package from stat_rolling to stat_roller

---
+++ app/src/main/java/guidemon/engine/stat_roller/strategies/StraightGachaRollStrategy.java
@@ -0,0 +1,35 @@
+package guidemon.engine.stat_roller.strategies;
+
+import java.util.Random;
+
+import guidemon.engine.stat_roller.IStatRollingStrategy;
+
+/* 1. Roll 1d20
+ * 2. Take this value for each ability score 
+ */
+public class StraightGachaRollStrategy implements IStatRollingStrategy {
+    @Override
+    public int[] rollStats() {
+        int[] stats = new int[6]; 
+
+        Random rand = new Random(); 
+        int max = 20; 
+        int min = 1; 
+
+        for (int i = 0 ; stats.length > i ; i++) {
+            stats[i] = rand.nextInt((max - min + 1) + min); 
+        }
+
+        return stats; 
+    } 
+
+    @Override
+    public String getName() {
+        return "Straight Gacha Roll"; 
+    }
+
+    @Override
+    public String toString() {
+        return getName(); 
+    }
+}
