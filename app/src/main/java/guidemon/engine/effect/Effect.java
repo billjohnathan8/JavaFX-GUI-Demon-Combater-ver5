6fe3f12a442507de8b0ff40d3eb5505a124222ca
BarracudaBill <paladingod8@gmail.com>
2025-07-03 14:08:16 +0800

    implemented skeleton of Effect for Effect used in Ability Feature

---
+++ app/src/main/java/guidemon/engine/effect/Effect.java
@@ -0,0 +1,42 @@
+package guidemon.engine.effect;
+
+import java.util.List; 
+
+import guidemon.engine.scene.SceneContext; 
+import guidemon.model.actor.Actor; 
+
+/*
+ * 
+ * Belongs on and is added to an Entry
+ * 
+ * It can be applied during or outside of combat via Active Abilities. 
+ * 
+ * It is the bulk of what makes an Entry Unique and is what Abilities use to affect another entry. 
+ * 
+ * This class is the common interface used by all effects. 
+ */
+public interface Effect {
+    
+    /**
+     * Apply this effect to the target(s) in the given context.
+     * The single entry point: apply whatever logic yoyu need.
+     * 
+     * Append and apply the effects to the actors directly. 
+     * 
+     * Passive Effects - always active given certain conditions.
+     * Active Effects - one-time-use, casting has conditions, not the effect itself. <- Usually applied to combatants via Attack or other Active Abilities. 
+     * @param actor the ability's caster (null for passive startup) - who casted the ability
+     * @param targets list of affected actors
+     * @param context the shared context (events, spaceManager, timeManager, roster) - holds SpaceManager, TimeManager, EventBus, etc.
+     */
+    void apply(Actor actor, List<Actor> targets, SceneContext context); 
+}
+
+/*
+Conditional: Apply to only ability scores
+Conditional: Apply to only primary resources
+Conditional: Apply to only secondary resources
+Conditional: Apply to only specified rostered resources
+Conditional: Apply to all rolls
+Conditional: Apply to only specified rolls
+ */
