f15a0c4d49b1e5a5baf0c66aa5fef3ff1de1b93f
BarracudaBill <paladingod8@gmail.com>
2025-07-08 01:51:31 +0800

    added skeleton for detection and stealth

---
+++ app/src/main/java/guidemon/model/vision/detection/DetectedEntry.java
@@ -0,0 +1,13 @@
+package guidemon.model.vision.detection;
+
+import guidemon.model.entry.Entry; 
+
+//TODO: 
+public class DetectedEntry extends Entry {
+    //corresponding viewer - privilege of access to display information. 
+
+    public DetectedEntry(String name) {
+        super(name); 
+    }
+    
+}
